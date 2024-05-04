ce10c25dbf54a1448fdbfd7fef8c1bbec74f4255
Ishaan <faded@kali.fadedstarboy.com>
2024-05-04 06:20:21 +0530

    login/logout user completed

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loggedOutUser, loginUser, registerUser } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js"   
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -15,5 +16,9 @@ router.route("/register").post(
     }]),
     registerUser)
 
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, loggedOutUser)
 
 export default router
