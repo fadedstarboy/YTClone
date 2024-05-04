cbe8f0206b74137ff1988488a3fc1d117d9a61a1
Ishaan <faded@kali.fadedstarboy.com>
2024-05-04 12:37:12 +0530

    user can now update profile

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loggedOutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loggedOutUser, loginUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js"   
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,5 +22,17 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, loggedOutUser)
 
 router.route("/refreshtoken").post(refreshAccessToken)
+
+router.route("/changepassword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/currentuser").get(verifyJWT, getCurrentUser)
+
+router.route("/updateaccount").patch(verifyJWT, updateAccountDetails)
+
+router.route("/updateavatar").patch(verifyJWT, upload.single("avatar"), updateAccountDetails)
+
+router.route("/updatecover").patch(verifyJWT, upload.single("coverImage"), updateAccountDetails)
+
+
 
 export default router
